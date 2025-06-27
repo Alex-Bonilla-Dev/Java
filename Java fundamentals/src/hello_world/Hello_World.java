b8c5eeb92a3e437771516f734c0f71ff1427fcae
Alex-Bonilla-Dev <alexbonillatrabajo@gmail.com>
2025-06-27 12:06:46 +0200

    First proyect that print hello world

---
+++ Java fundamentals/src/hello_world/Hello_World.java
@@ -0,0 +1,18 @@
+package hello_world;
+
+/**
+ *
+ * @author Alex Bonilla
+ */
+public class Hello_World {
+
+    /**
+     * @param args Command line arguments (this not used in this example)
+     */
+    public static void main(String[] args) {
+        // Prints Hello world message to the console.
+        System.out.println("Hello world");
+    
+    }
+    
+}
