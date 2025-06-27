ed406f10a1e1f32b0385ea924c5d9c45fbaeeac4
Alex-Bonilla-Dev <alexbonillatrabajo@gmail.com>
2025-06-27 12:26:12 +0200

    Hello world

---
+++ Java fundamentals/src/Hello_World/Hellor_World.java
@@ -0,0 +1,17 @@
+package Hello_World;
+
+/**
+ *
+ * @author Alex Bonilla
+ */
+public class Hellor_World {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Prints "Hello World" to the console
+        System.out.println("Hello World");
+    }
+
+}
